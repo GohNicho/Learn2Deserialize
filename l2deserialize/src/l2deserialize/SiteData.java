cac77338914da94fd5ba557b0adfb2407321b2d0
Nicholas Goh <n.goh@hotmail.com>
2014-03-01 01:18:38 -0500

    Finished raw classes for deserialization
    Does not compile

--- l2deserialize/src/l2deserialize/SiteData.java
+++ l2deserialize/src/l2deserialize/SiteData.java
@@ -1,30 +1,43 @@
 package l2deserialize;
 
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
 
 @Root
 public class SiteData {
 
-    public class Location {
-        // TODO
+    @ElementList(inline = true)
+    public list<dateTime> dateTimes {
+
     }
 
+    @Element
+    public class Location {
+
+    }
+
+    @Element
     public class CurrentConditions {
 
     }
 
+    @Element
     public class ForecastGroup {
 
     }
 
+    @Element
     public class YesterdayConditions {
 
     }
 
+    @Element
     public class RiseSet {
 
     }
 
+    @Element
     public class Almanac {
         
     }
